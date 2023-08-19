914f50a3d95e1e63710aedd54f3cc2f33b420190
s3nkwr <96329203+s3nkwr@users.noreply.github.com>
2023-08-19 17:02:56 +0400

    Delete addons/vanish directory

--- addons/vanish/src/main/java/xyz/jpenilla/squaremap/addon/vanish/VanishAdapter.java
+++
@@ -1,9 +0,0 @@
-package xyz.jpenilla.squaremap.addon.vanish;
-
-import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
-
-public interface VanishAdapter extends Listener {
-
-    boolean isVanished(final Player player);
-}
